7b0519ced4515382c3d58fc325a42aaa38f41292
Jay Manhire <jaymanhire@gmail.com>
2019-01-03 02:51:05 -0500

    Interfaces have been made generic.

--- Lambda_Ready_48/src/App.java
+++ Lambda_Ready_48/src/App.java
@@ -2,54 +2,56 @@
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		Human tom = new Human();
-		//walker(tom);
+		// walker(tom);
 
 		Robot wale = new Robot();
 		// walker(wale);
 
-		// This lambda is an implementation of the Walkable interface		
-		// Anonymous implementation of an abstract method listed in the functional interface
-		walker(() ->System.out.println("Custom object walking..."));
-		
-		//Lambdas type must be a functional interface-> an interface with 1 abstract method.
+		// This lambda is an implementation of the Walkable interface
+		// Anonymous implementation of an abstract method listed in the
+		// functional interface
+		walker(() -> System.out.println("Custom object walking..."));
+
+		// Lambdas type must be a functional interface-> an interface with 1
+		// abstract method.
 		Walkable aBlockOfCode = () -> {
 			System.out.println("Custom object walking...");
 			System.out.println("The object tripped...");
 		};
 		walker(aBlockOfCode);
-		
-		ALambdaInterface helloVar = ()->  System.out.println("hello there, I'm a lambda expression!");
+
+		ALambdaInterface helloVar = () -> System.out.println("hello there, I'm a lambda expression!");
 		Calculate sumVar = (a, b) -> a + b;
 		helloVar.someMethod();
 		System.out.println(sumVar.compute(4, 6));
-		
+
 		Calculate nonZeroDivider = (a, b) -> {
 			if (a == 0) {
 				return 0;
-			} 
-				return a / b;
+			}
+			return a / b;
 		};
-		System.out.println(nonZeroDivider.compute(40, 20));		
-		
-		StrReverse strRev = (str) -> {
+		System.out.println(nonZeroDivider.compute(40, 20));
+
+		MyGenericInterface<String> strRev = (s) -> {
 			String result = "";
-			for (int i = str.length() - 1; i >= 0; i--) {
-				result = result + str.charAt(i);
+			for (int i = s.length() - 1; i >= 0; i--) {
+				result = result + s.charAt(i);
 			}
 			return result;
 		};
-		System.out.println("'Vehicle' reversed: " + strRev.stringRev("Vehicle"));
-		
-		FactorialFinder factorial = (number) -> {
+		System.out.println("'Vehicle' reversed: " + strRev.work("Vehicle"));
+
+		MyGenericInterface<Integer> factorial = (number) -> {
 			int result = 1;
 			for (int i = result; i <= number; i++) {
-				result = result * i;		
+				result = result * i;
 			}
 			return result;
 		};
-		System.out.println("5 factorial: "  + factorial.fact(6));
+		System.out.println("5 factorial: " + factorial.work(6));
 	}
 
 	public void sayHello() {
@@ -61,7 +63,7 @@ public class App {
 	}
 
 	public int nonZeroDivide(int arg1, int arg2) {
-		if (arg1 == 0 ) {
+		if (arg1 == 0) {
 			return 0;
 		}
 		return arg1 / arg2;
@@ -90,11 +92,9 @@ public class App {
 	interface Calculate {
 		public int compute(int a, int b);
 	}
-	interface StrReverse {
-		public String stringRev(String str); 
-	}
-	interface FactorialFinder {
-		public int fact(int number);
+
+	interface MyGenericInterface<T> {
+		public T work(T t);
 	}
 
 }

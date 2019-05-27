183496dc8c892672abd37733e6820d42f88d3752
Jay Manhire <jaymanhire@gmail.com>
2019-05-26 22:32:38 -0400

    The Stack to reverse a String

---
+++ Data_Structures_JR2/src/ds/stack/stringreverse/App.java
@@ -0,0 +1,21 @@
+package ds.stack.stringreverse;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Stack myStack = new Stack(3);
+
+		myStack.push(20);
+		myStack.push(40);
+		myStack.push(60);
+		myStack.push(80);
+
+		while (!myStack.isEmpty()) {
+			long value = myStack.pop();
+			System.out.println(value);
+		}
+
+	}
+
+}

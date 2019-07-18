424210e18fd1029c7c2026d99b07c5aaa17af5e9
Jay Manhire <jaymanhire@gmail.com>
2019-07-18 19:18:00 -0400

    Spring-Data-Rest-CRUD

---
+++ 24-spring-data-rest-crud/src/main/java/com/jlm/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.jlm.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.jlm.springboot.cruddemo.entity.Employee;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// no need for an Impl class
+
+}

5f0d1b1a2e8489fe12f0235cd91edf80474487a8
Jay Manhire <jaymanhire@gmail.com>
2019-07-31 10:09:51 -0400

    Dependency_Injection with Spring

--- Java_Interview_1/src/com/jlm/stringequality/Main.java
+++ Java_Interview_1/src/com/jlm/stringequality/Main.java
@@ -1,26 +1,62 @@
 package com.jlm.stringequality;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
+	static boolean isAnagram(String a, String b) {
+		// Complete the function
+
+		if (a == null || b == null || a.equals("") || b.equals(""))
+			throw new IllegalArgumentException();
+
+		if (a.length() != b.length())
+			return false;
+
+		a = a.toLowerCase();
+		b = b.toLowerCase();
 		
-		String literal = "hello";
-		
-		String obj = new String("hello");
-		
-		String otherLiteral = "hello";
-		
-		String otherObj = new String("hello");
-		
-		System.out.println(literal == obj);
-		System.out.println(literal == otherLiteral);
-		System.out.println(literal.equals(obj));
-		System.out.println(literal.equals(otherLiteral));
-		
-		System.out.println();
-		System.out.println(obj == otherObj);
-		System.out.println(obj.equals(otherObj));
+		/* Populate HashMap with String a */
+		Map<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < b.length(); i++) {
+			char letter = b.charAt(i);
+
+			if (!map.containsKey(letter)) {
+				map.put(letter, 1);
+			} else {
+				Integer freq = map.get(letter);
+				map.put(letter, freq++);
+			}
+		}
+		/* Compare String b to a's HashMap */
+		for (int k = 0; k < a.length(); k++) {
+			char letter = a.charAt(k);
+
+			if (!map.containsKey(letter))
+				return false;
+
+			Integer frequency = map.get(letter);
+
+			if (frequency == 0)
+				return false;
+			else
+				map.put(letter, --frequency);
+		}
+		// if the code got that far it is an anagram
+		return true;
 
 	}
 
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		boolean ret = isAnagram(a, b);
+		System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+	}
 }

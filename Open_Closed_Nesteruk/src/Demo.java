ffbd3f8566212623422562f6e2b289f7efb3fb94
Jay Manhire <jaymanhire@gmail.com>
2019-08-23 13:13:52 -0400

    Open-Closed with Specification

---
+++ Open_Closed_Nesteruk/src/Demo.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+		Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+		Product house = new Product("House", Color.BLUE, Size.LARGE);
+
+		List<Product> products = List.of(apple, tree, house); // Need at least Java 9
+
+		// List<Product> products = Arrays.asList(apple, tree, house);
+
+		ProductFilter pf = new ProductFilter();
+		System.out.println("Green products (old):");
+		pf.filterByColor(products, Color.GREEN).forEach(p -> System.out.println("-" + p.name + " is green"));
+
+		System.out.println();
+
+		BetterFilter bf = new BetterFilter();
+		System.out.println("Green products (new):");
+		bf.filter(products, new ColorSpecification(Color.GREEN))
+				.forEach(p -> System.out.println("-" + p.name + " is green"));
+
+		System.out.println();
+
+		System.out.println("Large blue items: ");
+		bf.filter(products,
+				new AndSpecification<>(new ColorSpecification(Color.BLUE), new SizeSpecification(Size.LARGE)))
+				.forEach(p -> System.out.println("-" + p.name + " is large and blue."));
+
+	}
+
+}
